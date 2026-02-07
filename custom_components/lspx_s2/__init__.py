4ded34ce9d966c5ee83a5be30d61157191052928
vaivaitm <qwerty1994tmpublic@gmail.com>
2026-02-07 18:42:12 +0900

    Add LSPX-S2 custom component with configuration and media player support

---
+++ custom_components/lspx_s2/__init__.py
@@ -0,0 +1,51 @@
+"""The LSPX-S2 custom component."""
+
+import voluptuous as vol
+
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+from homeassistant.const import CONF_NAME, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.typing import ConfigType
+
+from .const import CONF_ENDPOINT, DOMAIN
+from .services import async_setup_services
+
+LSPX_S2_CONFIG_SCHEMA = vol.Schema(
+    {vol.Optional(CONF_NAME): cv.string, vol.Required(CONF_ENDPOINT): cv.string}
+)
+
+CONFIG_SCHEMA = vol.Schema(
+    {vol.Optional(DOMAIN): vol.All(cv.ensure_list, [LSPX_S2_CONFIG_SCHEMA])},
+    extra=vol.ALLOW_EXTRA,
+)
+
+PLATFORMS = [Platform.MEDIA_PLAYER, Platform.LIGHT]
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up LSPX-S2 environment."""
+    async_setup_services(hass)
+
+    if (conf := config.get(DOMAIN)) is None:
+        return True
+    for config_entry in conf:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN,
+                context={"source": SOURCE_IMPORT},
+                data=config_entry,
+            ),
+        )
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up LSPX-S2 media player."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload LSPX-S2 media player."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
---
+++ custom_components/lspx_s2/const.py
@@ -0,0 +1,7 @@
+"""Constants for the LSPX-S2 component."""
+
+DOMAIN = "lspx_s2"
+
+CONF_ENDPOINT = "endpoint"
+
+ERROR_REQUEST_RETRY = 40000
